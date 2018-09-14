285719d39ddc63008404c0318eebff545530a0ef
sepuckett86 <sepuckett86@gmail.com>
2018-09-14 16:51:30 +0200

    add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Navbar from 'components/Navbar';
 import About from 'components/About';
 import Intro from 'components/Intro';
 
 export default () => {
   return(
     <div>
-      Hello, this is App.
-      <Route path="/about" component={About} />
-      <Route path="/intro" component={Intro} />
+      <Navbar />
+      <div className='container'> Hello, this is App.
+        <Route path="/about" component={About} />
+        <Route path="/intro" component={Intro} />
+      </div>
     </div>
   );
 };
