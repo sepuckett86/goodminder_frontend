8daf19d1a8966ecde768e09be5e7cf12a1c2e7df
sepuckett86 <sepuckett86@gmail.com>
2018-09-14 15:20:13 +0200

    add components and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import About from 'components/About';
+import Intro from 'components/Intro';
+
 export default () => {
   return(
     <div>
       Hello, this is App.
+      <About />
+      <Intro />
     </div>
   );
 };
