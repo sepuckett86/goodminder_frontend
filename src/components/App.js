a56bb7a3245aaab27a423559b671c02c4caa8866
sepuckett86 <sepuckett86@gmail.com>
2018-09-19 18:08:56 +0200

    convert back to bootstrap 4

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Navbar from 'components/Navbar';
 import About from 'components/About';
 import Intro from 'components/Intro';
+import Home from 'components/Home';
 
 export default () => {
   return(
@@ -12,6 +13,7 @@ export default () => {
       <div>
         <Route path="/about" component={About} />
         <Route path="/intro" component={Intro} />
+        <Route path="/" exact component={Home} />
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import logo from 'images/logo.png';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default() => {
   return (
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/"><img src={logo} alt="logo"/>Goodminder</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="/about">
-            About
-          </NavItem>
-          <NavItem eventKey={2} href="/intro">
-            Intro
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavDropdown eventKey={1} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={1.1}>Action</MenuItem>
-            <MenuItem eventKey={1.2}>Another action</MenuItem>
-            <MenuItem eventKey={1.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={1.3}>Separated link</MenuItem>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <header id='header' className="App-header">
+            <nav className="navbar navbar-dark navbar-expand-sm justify-content-between">
+              <a className="navbar-brand brand-font" id='intro' href="/intro">
+              <img src={logo} className="App-logo" alt="logo"/>
+                  Goodminder
+                  </a>
+              <div className="navbar-expand" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item dropdown">
+                    <button className="nav-link paragraph-font btn btn-clean menu dropdown-toggle" type="button" id="dropdownMenuButton"
+                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        UserID
+                    </button>
+                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                      <a className="dropdown-item" href="/"><b>Home</b></a>
+                      <a className="dropdown-item" href="/settings">Settings</a>
+                      <div className="dropdown-divider"></div>
+                      <a className="dropdown-item" href="/about">About</a>
+                      <a className="dropdown-item" href="/examples">Examples</a>
+                      <a className="dropdown-item" href="/faq">FAQ</a>
+                      <a className="dropdown-item" href="/contact">Contact</a>
+                      <div className="dropdown-divider"></div>
+                      <a className="dropdown-item" href="/logout">Log out</a>
+                    </div>
+                  </li>
+                  <li className="nav-item points">
+                    <button type="button" className="btn btn-goodminder" data-toggle="popover" title="Gminder Points" data-content="Earn points by daily log-in and writing entries. These will come in handy later :) ">
+                      {' '}<span className="badge badge-light">40</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </header>
 )
 };
